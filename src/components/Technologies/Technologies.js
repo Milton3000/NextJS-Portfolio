b97e427b6a5984489f3b55d0489bd5a7aa3f22d1
MK3000 <kristofferssonmilton@gmail.com>
2024-03-08 15:02:34 +0100

    TimeLine Carousel, Section, ScrollNode

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -6,6 +6,7 @@ import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './Techn
 const Technologies = () =>  (
 <Section id="tech">
 <SectionDivider />
+<br />
 <SectionTitle>Technologies</SectionTitle>
 <SectionText>
   I've worked with a range of technologies in the web development world.
