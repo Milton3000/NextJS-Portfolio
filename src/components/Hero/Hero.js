0809dc3104f9b34bd1be3e50ff56a2cd34eeeada
MK3000 <kristofferssonmilton@gmail.com>
2024-03-08 09:39:19 +0100

    Styles, Components, Hero, Navbar

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+<Section row nopadding>
+<LeftSection>
+  <SectionTitle main center>
+Welcome To <br/> 
+My Personal Portfolio
+  </SectionTitle>
+  <SectionText>
+    TestTestTestTestTestTestTestTestTestTestTestTestTestTestTest
+  </SectionText>
+  <Button onClick={() => window.location = 'https://google.com'}>
+    Learn More
+  </Button>
+</LeftSection>
+</Section>
+);
+
+export default Hero;
