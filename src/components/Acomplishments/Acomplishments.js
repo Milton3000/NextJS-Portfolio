68545f4969eb8634550400d59d4d8684aa4e57d7
MK3000 <kristofferssonmilton@gmail.com>
2024-03-10 00:42:35 +0100

    Added Luna Image in Accomplishments

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,33 +1,32 @@
 import React from 'react';
-
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
-
 
 // Can move to constants folder later, but it's quite short.
 const data = [
   { number: 5, text: 'Open Source Projects'},
   { number: 4, text: 'Certficates', },
   { number: 2, text: 'Github Followers', },
-  { number: 1, text: 'Shiba Inu', }
+  { number: 1, text: 'Shiba Inu', icon: '/images/luna.png' } // Changed the icon filename to 'luna.png'
 ];
 
 const Acomplishments = () => (
-<Section>
-  <SectionTitle>
-    Personal Accomplishments
-  </SectionTitle>
-  <Boxes>
-    {data.map((card, index) => (
-      <Box key={index}>
-<BoxNum>
-  {card.number}+
-</BoxNum>
-<BoxText>{card.text}</BoxText>
-      </Box>
-    ))}
-  </Boxes>
-</Section>
+  <Section>
+    <SectionTitle>
+      Personal Accomplishments
+    </SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>
+            {card.number}+
+          </BoxNum>
+          <BoxText>{card.text}</BoxText>
+          {index === data.length - 1 && card.icon && <img src={card.icon} alt="luna" style={{ maxWidth: '40px', maxHeight: '40px', marginLeft: '160px' }} />} {/* Add this line to conditionally render the image and set its maximum width and height */}
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
