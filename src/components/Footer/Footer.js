ca26d9e4db088341b925c102f7bb45a066ed5650
MK3000 <kristofferssonmilton@gmail.com>
2024-03-09 11:10:37 +0100

    Footer Links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+<FooterWrapper>
+  <LinkList>
+  <LinkColumn>
+  <LinkTitle>
+  Call
+  </LinkTitle>
+<LinkItem href='tel: +46709457651'>
++46709457651
+</LinkItem>
+  </LinkColumn>
+  <LinkColumn>
+  <LinkTitle>
+  Email
+  </LinkTitle>
+<LinkItem href='mailto:kristofferssonmilton@gmail.com'>
+kristofferssonmilton@gmail.com
+</LinkItem>
+  </LinkColumn>
+  </LinkList>
+  <SocialIconsContainer>
+    <CompanyContainer>
+      <Slogan>
+        Innovating one project at a time.
+      </Slogan>
+    </CompanyContainer>
+    
+  </SocialIconsContainer>
+</FooterWrapper>
   );
 };
 
